4aee94d5118163e0f737679258b6b01aa3ed7390
‘yuto’_0503 <y.yuto.0503@gmail.comgit config --global user.name yuto_0503git config --global user.email y.yuto.0503@gmail.com>
2024-02-22 09:57:13 +0900

    done12

--- src/main/java/com/example/repository/AdministratorRepository.java
+++ src/main/java/com/example/repository/AdministratorRepository.java
@@ -58,8 +58,7 @@ public class AdministratorRepository {
 	 * @return 管理者情報 存在しない場合はnullを返します
 	 */
 	public Administrator findByMailAddressAndPassward(String mailAddress, String password) {
-		String sql = "select id,name,mail_address,password from administrators where mail_address= '" + mailAddress
-				+ "' and password='" + password + "'";
+		String sql = "select id,name,mail_address,password from administrators where mail_address=:mailAddress and password=:password";
 		SqlParameterSource param = new MapSqlParameterSource();
 		List<Administrator> administratorList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
 		if (administratorList.size() == 0) {
